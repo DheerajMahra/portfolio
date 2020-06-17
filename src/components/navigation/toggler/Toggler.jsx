d1db4588fbbe1f227e7b00547edfe1ba817de646
dheeraj <dheerajmahra873@gmail.com>
2020-06-17 15:29:19 +0530

    updated toggler icon

--- src/components/navigation/toggler/Toggler.jsx
+++ src/components/navigation/toggler/Toggler.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Toggler.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLinkAlt, faGripLines, faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Toggler = () => {
     return (
         <>
         <input className="Navigation__checkbox" type="checkbox" id="nav-toggler" />
-        <label className="Navigation__toggler" htmlFor="nav-toggler">+</label>
+        <label className="Navigation__toggler" htmlFor="nav-toggler">
+            <FontAwesomeIcon className="Navigation__icon" icon={faBars} /> 
+        </label>
         </>
     )
 }
