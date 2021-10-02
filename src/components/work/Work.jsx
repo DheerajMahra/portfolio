ea31745773480ab188c3ec696de8989898e9e86f
Dheeraj Mahra <dheerajmahra873@gmail.com>
2021-10-02 14:57:42 +0530

    Added github link in work section end

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -10,6 +10,14 @@ const Work = () => {
             <SectionTitle>My Work</SectionTitle>
             <div className="Work-container">
                 <WorkList />
+                <div className="u-tc">
+                    <a href="https://github.com/DheerajMahra" target="_blank" rel="noopener noreferrer">
+                        <p className="Work__strong u-tc">
+                            Still not impressed? Check GitHub
+                            <span role="img" aria-label="go to github">🤘</span>
+                        </p>
+                    </a>
+                </div>
             </div>
         </div>
     )
